493f351fc636a0a849fea6992d6673749181cdca
faith <jamienibtong@gmail.com>
2025-07-17 17:22:29 +0800

    fix cors

--- backend/src/apple_dl/routes/websocket.py
+++ backend/src/apple_dl/routes/websocket.py
@@ -18,7 +18,8 @@ sio: TypedAsyncServer = cast(
         logger=False,
         engineio_logger=False,
         cors_allowed_origins=[
-            "ws://127.0.0.1:5000",
+            "http://localhost:5001",
+            "http://127.0.0.1:5001",
             "http://127.0.0.1:5000",
             "http://localhost:6887",
         ],
